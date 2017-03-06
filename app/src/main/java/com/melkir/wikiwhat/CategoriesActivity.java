7d9ba4b4f33573f8a69c99b2dca4d3d304de45cd
melkir <melkir13@gmail.com>
2017-03-06 14:14:45 +0100

    Initial commit

---
+++ app/src/main/java/com/melkir/wikiwhat/CategoriesActivity.java
@@ -0,0 +1,13 @@
+package com.melkir.wikiwhat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CategoriesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.categories_activities);
+    }
+}
