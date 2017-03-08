2215f231d1706ebe66db1ad02e1d5054fe1df81e
melkir <melkir13@gmail.com>
2017-03-08 15:29:25 +0100

    (test) refractor tests

--- app/src/test/java/com/melkir/wikiwhat/categories/CategoriesRepositoryTest.java
+++ app/src/test/java/com/melkir/wikiwhat/categories/CategoriesRepositoryTest.java
@@ -8,34 +8,42 @@ import org.junit.Test;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 
+import io.reactivex.subscribers.TestSubscriber;
+
 public class CategoriesRepositoryTest {
     @Rule
     public MockitoRule mockitoRule = MockitoJUnit.rule();
     private CategoriesRepository mCategoriesRepo;
+    private TestSubscriber<String> testSubscriber;
 
     @Before
     public void setUp() {
         mCategoriesRepo = new CategoriesRepository();
+        testSubscriber = new TestSubscriber<>();
     }
 
     @Test
     public void shouldFetchPageFromWipedia() {
         // TODO Fix this method
-        mCategoriesRepo.getPageContent(370096).subscribe(System.out::println, System.out::println);
+        mCategoriesRepo.getPageContent(370096).subscribe();
+        testSubscriber.assertNoErrors();
     }
 
     @Test
     public void shouldFetchCategoryMembers() {
-        mCategoriesRepo.getCategoryMembers(40924578, 3).subscribe(System.out::println, System.out::println);
+        mCategoriesRepo.getCategoryMembers(40924578, 3).subscribe();
+        testSubscriber.assertNoErrors();
     }
 
     @Test
     public void shouldFetchRandomCategoriesFromWikipedia() {
-        mCategoriesRepo.getRandomCategories(3).subscribe(System.out::println, System.out::println);
+        mCategoriesRepo.getRandomCategories(3).subscribe();
+        testSubscriber.assertNoErrors();
     }
 
     @Test
     public void shouldFetchRandomCategoryFromWikipedia() {
-        mCategoriesRepo.getRandomCategory().subscribe(System.out::println, System.out::println);
+        mCategoriesRepo.getRandomCategory().subscribe();
+        testSubscriber.assertNoErrors();
     }
 }
