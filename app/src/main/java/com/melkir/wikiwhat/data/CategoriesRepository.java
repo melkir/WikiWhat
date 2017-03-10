524e6e733ab8814a39754a58001a87d8afcf8259
melkir <melkir13@gmail.com>
2017-03-10 02:05:09 +0100

    (fix) missing import

--- app/src/main/java/com/melkir/wikiwhat/data/CategoriesRepository.java
+++ app/src/main/java/com/melkir/wikiwhat/data/CategoriesRepository.java
@@ -19,6 +19,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import io.reactivex.Observable;
+import io.reactivex.schedulers.Schedulers;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
